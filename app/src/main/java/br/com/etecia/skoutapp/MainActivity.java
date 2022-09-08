a40704e70507290ee80d6718f6d6476347d3fced
DaniloHY <nilo.11.dhy@gmail.com>
2022-09-08 20:22:07 -0300

    aula2

--- app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/skoutapp/MainActivity.java
@@ -3,12 +3,33 @@ package br.com.etecia.skoutapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    // Declarar as variáveis globais que irão representar os objetos xml
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Apresentar ao java os componentes do xml
+        edtSenha = findViewById(R.id.edtSenha);
+        edtUsuario = findViewById(R.id.edtUsuario);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
